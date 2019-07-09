3793d0b84aad36e3400b2f31a5a276b8a26ac4a8
Yousef Mutawe <ymotawe@tacme.com>
2019-07-09 12:01:56 +0400

    Initial Commit

---
+++ MAFNews/MAFNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAFNews
+//
+//  Created by Yousef Mutawe on 7/9/19.
+//  Copyright © 2019 Motawe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
