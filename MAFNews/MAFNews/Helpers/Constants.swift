c75ef263bb921c856f4bbea67a42e7cf534505b5
Yousef Mutawe <ymotawe@tacme.com>
2019-07-09 15:05:30 +0400

    Pods added to the project

---
+++ MAFNews/MAFNews/Helpers/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  MAFNews
+//
+//  Created by Yousef Mutawe on 7/9/19.
+//  Copyright © 2019 Motawe. All rights reserved.
+//
+
+import Foundation
+let kAPIKey = "756cd5a0a2954a43ae415240bf73966c"
